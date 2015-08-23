9706cc87e90f774dbdf4ca22541d5bab0a5a3edd
Sequina Caro <sccaro89@gmail.com>
2015-08-22 21:30:28 -0500

    textbox/button function

---
+++ java2.js
@@ -0,0 +1,26 @@
+var Holiday = (function () {
+	var langGer = {
+     "merry":"Frohe", 
+     var xmas = "christmas":"Weihnachten"; 
+     var und = "and":"und"; 
+     var expression = "happy":"ein glückliches"; 
+     var news = "new":"neues";
+     var 12mons = "year":"Jahr";     
+};
+
+	return {
+		translateTo: new Word(german),
+		getLangGer: function() {
+			return langGer; 
+		},
+
+
+
+
+
+
+
+	}
+
+
+}());
---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+
+var textbox = "";
+
+function german(Holiday) {
+	for (var i = 0; i < german.length; i++){
+		var textbox = Holiday[i];
+		console.log(textbox)
+	}
+}
+
+
+
+$("#langTranslator").click(function() {
+$.ajax({
+	url: "./java2.js"
+}).done(function(Holiday) {
+
+		)};
+
+	)};
+
+)};
